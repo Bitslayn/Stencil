4d0f8678c91dac85e632634104506168b5df8f94
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2026-05-07 11:37:41 -0400

    Add checkbox behavior and events

--- widget/presets/input/checkbox.lua
+++ widget/presets/input/checkbox.lua
@@ -8,20 +8,30 @@ return function(class, super, elem)
 	---@class FOXStencil.Widgets.Checkbox: FOXStencil.Widgets.Generic
 	---@field setProps fun(self: self, props: FOXStencil.Widgets.Checkbox.Props, group: FOXStencil.Element.Props.Group?): self
 	---@field getProps fun(self: self, group: FOXStencil.Element.Props.Group?): FOXStencil.Widgets.Checkbox.Props
+	---@field toggle fun(self: FOXStencil.Widgets.Checkbox, state: boolean)?
+	---@field toggled boolean
+	---@field id string
 	class = class
 
 	---@class FOXStencil.Element
 	elem = elem
 
-	-- Create function registered to element superclass
-	-- Be sure to change this
+	---@type table<string, FOXStencil.Widgets.Checkbox[]>
+	local groups = {}
 
 	---@param props FOXStencil.Widgets.Checkbox.Props?
+	---@param id string?
 	---@return FOXStencil.Widgets.Checkbox
-	function elem:newCheckbox(props)
+	function elem:newCheckbox(props, id)
 		local widg = self:newElement() --[[@as FOXStencil.Widgets.Checkbox]]
 
-		local toggled = false
+		if type(id) == "string" then
+			if not groups[id] then
+				groups[id] = {}
+			end
+			table.insert(groups[id], widg)
+			widg.id = id
+		end
 
 		widg:setProps({
 			size = vec(10, 10),
@@ -39,24 +49,68 @@ return function(class, super, elem)
 			hover = function(_, rel_pos, true_pos, sound_pos, state, changed) end,
 			click = function(_, rel_pos, true_pos, sound_pos, state)
 				if not state then return end
-				toggled = not toggled
 
-				if toggled then
-					widg:setProps({
-						tex_color = vectors.hexToRGB("blue"),
-						tex_uv_pos = vec(4, 0),
-					})
-				else
-					widg:setProps({
-						tex_color = vec(0.5, 0.5, 0.5),
-						tex_uv_pos = vec(4, 4),
-					})
-				end
+				sounds:playSound(
+					"minecraft:block.lava.pop",
+					sound_pos,
+					1,
+					widg.toggled and 8 or 9 -- Dev note: the toggled state is inverse to what's being applied
+				)
+
+				widg:setToggled(not widg.toggled)
 			end,
 		}):setProps(props or {})
 
 		widg:setProps({ border = vec(1, 1, 1, 1) }, "hover")
 
 		return setmetatable(widg, class)
+	end
+
+	---@param func fun(self: FOXStencil.Widgets.Checkbox)
+	---@return FOXStencil.Widgets.Checkbox
+	function class:onToggled(func)
+		self.toggle = func
+		return self
+	end
+
+	---@param state boolean
+	---@return FOXStencil.Widgets.Checkbox
+	function class:setToggled(state)
+		state = state == nil and false or state
+		if self.toggled == state then return self end
+		self.toggled = state
+
+		local group = groups[self.id]
+		if state and group then
+			for i = 1, #group do
+				if group[i] ~= self then
+					group[i]:setToggled(false)
+				end
+			end
+		end
+
+		-- Call function
+
+		if self.toggle then
+			self.toggle(self, self.toggled)
+		end
+
+		-- Update style
+
+		if self.toggled then
+			self:setProps({
+				tex_color = vectors.hexToRGB("blue"),
+				tex_uv_pos = vec(4, 0),
+			})
+		else
+			self:setProps({
+				tex_color = vec(0.5, 0.5, 0.5),
+				tex_uv_pos = vec(4, 4),
+			})
+		end
+
+		self:draw(true)
+
+		return self
 	end
 end
