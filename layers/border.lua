a995770e8093bc0bf44c79e45cc0e2499ebf686c
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2026-05-20 21:13:03 -0400

    Add new border layer

---
+++ layers/border.lua
@@ -0,0 +1,113 @@
+---@class FOXStencil.Border
+local obj = {}
+---@package
+obj.__index = obj
+
+---@class FOXStencil.Border.Styles
+local default = {
+	---@type Vector3|Vector4
+	color = vec(1, 1, 1, 1),
+
+	---@type Vector2
+	pos = vec(0, 0),
+	---@type Vector2
+	size = vec(0, 0),
+	---@type number
+	weight = 1,
+}
+---@package
+default.__index = default
+
+---Redraws this label
+---@param self FOXStencil.Border
+local function draw(self)
+	local styles = self.styles
+
+	local weight = styles.weight
+
+	local w_t, w_r, w_b, w_l = weight, weight, weight, weight
+
+	local w, h = styles.size:unpack()
+
+	local mats = {
+		-- Top
+		matrices.translate4(w_l, w_t, 0)
+		* matrices.scale4(w + w_l + w_r, w_t, 1),
+
+		-- Right
+		matrices.translate4(-w, 0, 0)
+		* matrices.scale4(w_r, h, 1),
+
+		-- Bottom
+		matrices.translate4(w_l, -h, 0)
+		* matrices.scale4(w + w_l + w_r, w_b, 1),
+
+		-- Left
+		matrices.translate4(w_l, 0, 0)
+		* matrices.scale4(w_l, h, 1),
+	}
+
+	for i = 1, 4 do
+		local visible = weight > 0
+
+		if visible then
+			self.tasks[i]
+				:matrix(matrices.translate4(-styles.pos:augmented(1 / 16)) * mats[i])
+				:color(styles.color)
+		end
+
+		self.tasks[i]:visible(visible)
+	end
+end
+
+---@generic v
+---@type table<type, fun(v: v): v>
+local copy = {
+	Vector2 = function(v) return v:copy() end,
+	Vector3 = function(v) return v:copy() end,
+	Vector4 = function(v) return v:copy() end,
+	number = function(v) return v end,
+}
+
+---Sets the given styles
+---@param styles FOXStencil.Border.Styles
+---@return self
+function obj:setStyles(styles)
+	local diff = false
+
+	for k, v in next, styles do
+		if self.styles[k] ~= v then
+			self.styles[k] = copy[type(v)](v)
+			diff = true
+		end
+	end
+
+	if diff then
+		draw(self)
+	end
+
+	return self
+end
+
+---Generates a label layer
+---
+---Call :setStyles() with a table to change the styles
+---@param part ModelPart
+---@return FOXStencil.Border
+return function(part)
+	---@type SpriteTask[]
+	local tasks = {}
+	---@class FOXStencil.Border
+	local self = {
+		tasks = tasks,
+		styles = setmetatable({}, { __index = default }),
+	}
+
+	for i = 1, 4 do
+		tasks[i] = part:newSprite("border-" .. math.random())
+			:texture(textures["FOXStencil_blank"], 1, 1)
+			:renderType("CUTOUT_EMISSIVE_SOLID")
+	end
+
+	return setmetatable(self, obj)
+end
