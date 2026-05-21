8f886abf99301f5895fd20faefb0f92cb0cf2df6
Bitslayn <spoongeblox@gmail.com>
2026-05-21 15:04:29 -0400

    Copy type assertion

--- common/copy.lua
+++ common/copy.lua
@@ -1,4 +1,13 @@
 ---@generic v
+---@param v v
+---@param msg string?
+---@param level integer?
+---@return v
+local function assert(v, msg, level)
+	return v or error(msg, level)
+end
+
+---@generic v
 ---@type table<type, fun(v: v): v>
 local copy_type = {
 	Vector2 = function(v) return v:copy() end,
@@ -18,7 +27,8 @@ return function(from, to)
 
 	for k, v in next, from do
 		if to[k] ~= v then
-			assert(copy_type[type(v)], "Unsupported type to copy " .. type(v))
+			assert(type(to[k]) == type(v), "Unexpected type to set on key " .. k .. ", " .. type(to[k]) .. " expected but found " .. type(v))
+			assert(copy_type[type(v)], "Unsupported type " .. type(v) .. " to copy")
 			to[k] = copy_type[type(v)](v)
 			diff = true
 		end
