d1e920d909b3254c37fe60a6f44e47801a6ac561
Bitslayn <bitslayn@outlook.com>
2026-07-01 03:23:02 -0400

    Add column widget and group container widgets

--- assets/widgets/containers/column.lua
+++ assets/widgets/containers/column.lua
@@ -5,10 +5,10 @@
 ---@diagnostic disable: invisible
 
 ---Generates a row widget
----@alias FOXStencil.Row.Generator fun(parent: FOXStencil.Element|FOXStencil.Screen, name: string, assets: FOXStencil.Assets): FOXStencil.Row
+---@alias FOXStencil.Column.Generator fun(parent: FOXStencil.Element|FOXStencil.Screen, name: string, assets: FOXStencil.Assets): FOXStencil.Row
 
 ---@class FOXStencil.Widgets
----@field row FOXStencil.Row.Generator
+---@field column FOXStencil.Column.Generator
 
 --#ENDREGION --=================================================================================================================
 --#REGION ˚♡ Builder ♡˚
@@ -17,7 +17,7 @@
 ---@param parent FOXStencil.Element|FOXStencil.Screen
 ---@param assets FOXStencil.Assets
 return function(parent, name, assets)
-	local elem = parent:newElement(name):setProps({ direction = "HORIZONTAL" })
+	local elem = parent:newElement(name):setProps({ size = vec(0, -1), align = vec(0.5, 0), gap = 2, direction = "VERTICAL" })
 
 	---@class FOXStencil.Row: FOXStencil.Element
 	local widg = {
---
+++ assets/widgets/containers/row.lua
@@ -0,0 +1,31 @@
+--==============================================================================================================================
+--#REGION ˚♡ Class ♡˚
+--==============================================================================================================================
+
+---@diagnostic disable: invisible
+
+---Generates a row widget
+---@alias FOXStencil.Row.Generator fun(parent: FOXStencil.Element|FOXStencil.Screen, name: string, assets: FOXStencil.Assets): FOXStencil.Row
+
+---@class FOXStencil.Widgets
+---@field row FOXStencil.Row.Generator
+
+--#ENDREGION --=================================================================================================================
+--#REGION ˚♡ Builder ♡˚
+--==============================================================================================================================
+
+---@param parent FOXStencil.Element|FOXStencil.Screen
+---@param assets FOXStencil.Assets
+return function(parent, name, assets)
+	local elem = parent:newElement(name):setProps({ size = vec(-1, 0), align = vec(0, 0.5), gap = 2, direction = "HORIZONTAL" })
+
+	---@class FOXStencil.Row: FOXStencil.Element
+	local widg = {
+		elem = elem,
+	}
+	elem.widg = widg
+
+	return setmetatable(widg, { __index = elem })
+end
+
+--#ENDREGION
